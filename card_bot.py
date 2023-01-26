402c9dcb98c95d7f333e6e1de723cb685abd472e
Zak <119809406+YungZak@users.noreply.github.com>
2023-01-26 21:14:04 +0300

    Add files via upload

---
+++ card_bot.py
@@ -0,0 +1,64 @@
+
+from telebot import TeleBot, types
+from faker import Faker
+
+bot = TeleBot(token='ТОКЕН', parse_mode='html') # создание бота
+
+faker = Faker() # утилита для генерации номеров кредитных карт
+
+# объект клавиаутры
+card_type_keybaord = types.ReplyKeyboardMarkup(resize_keyboard=True)
+# первый ряд кнопок
+card_type_keybaord.row(
+    types.KeyboardButton(text='VISA'),
+    types.KeyboardButton(text='Mastercard'),
+)
+
+# обработчик команды '/start'
+@bot.message_handler(commands=['start'])
+def start_command_handler(message: types.Message):
+    # отправляем ответ на команду '/start'
+    bot.send_message(
+        chat_id=message.chat.id, # id чата, в который необходимо направить сообщение
+        text='Йо! Я генерирую номер карты!\nВыбери тип карты:', # текст сообщения
+        reply_markup=card_type_keybaord,
+    )
+
+# обработчик всех остальных сообщений
+@bot.message_handler()
+def message_handler(message: types.Message):
+    # проверяем текст сообщения на совпадение с текстом какой либо из кнопок
+    # в зависимости от типа карты присваем занчение переменной 'card_type'
+    if message.text == 'VISA':
+        card_type = 'visa'
+    elif message.text == 'Mastercard':
+        card_type = 'mastercard'
+ 
+    else:
+        # если текст не совпал ни с одной из кнопок 
+        # выводим ошибку
+        bot.send_message(
+            chat_id=message.chat.id,
+            text='Не понимаю тебя :(',
+        )
+        return
+
+    # получаем номер тестовой карты выбранного типа
+    # card_type может принимать одно из зачений ['maestro', 'mastercard', 'visa13', 'visa16', 'visa19',
+    # 'amex', 'discover', 'diners', 'jcb15', 'jcb16']
+    card_number = faker.credit_card_number(card_type)
+    # и выводим пользователю
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=f'Тестовая карта {card_type}:\n<code>{card_number}</code>'
+    )
+
+
+# главная функция программы
+def main():
+    # запускаем нашего бота
+    bot.infinity_polling()
+
+
+if __name__ == '__main__':
+    main()
